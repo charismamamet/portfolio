c1be0afea92aabbc2ad50aa172438f1e538d2f0e
charismamamet <charisma.rahmat@gmail.com>
2025-07-03 22:13:38 +0700

    Add new folder

--- SQL Analytics/queries/commonItem.sql
+++
@@ -1,9 +0,0 @@
-select 
-	i.nameid,
-   	count(distinct i.char_id) as numOfDistinctOwner
-from new_rathena_db.inventory as i
-group by 
-	i.nameid
-order by numOfDistinctOwner desc
-limit 3
-;
--- SQL Analytics/queries/user_start_c.sql
+++
@@ -1,5 +0,0 @@
-select l.userid, l.sex, l.group_id, l.email
-from new_rathena_db.login as l
-where l.userid like "c%"
-order by l.group_id desc
-;
