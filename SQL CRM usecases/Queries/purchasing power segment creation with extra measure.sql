c16f7b48f2a9d4dcc8a8ef609330e16049abcf39
charismamamet <charisma.rahmat@gmail.com>
2025-07-27 20:23:40 +0700

    add modified purchasing power analysis

--- SQL CRM usecases/Queries/purchasing power segment creation with extra measure.sql
+++ SQL CRM usecases/Queries/purchasing power segment creation with extra measure.sql
@@ -29,21 +29,50 @@ FROM (
         SELECT o.user_id
         FROM mock_renos_db.mock_purchase AS o
         WHERE order_date < '2025-07-21'
-          AND order_status IN ('menunggu pembayaran', 'selesai')  -- only valid orders
-      )
+          AND order_status IN ('menunggu pembayaran', 'selesai')
+    )
     AND x.user_id NOT IN (
 		-- tabel consit of user_id of ppl who make purchase on or after 21 July 2025
         SELECT q.user_id
         FROM mock_renos_db.mock_purchase AS q
         WHERE order_date >= '2025-07-21'
-          AND order_status IN ('menunggu pembayaran', 'selesai')  -- exclude those who purchased again
+          AND order_status IN ('menunggu pembayaran', 'selesai')
     )
-    AND x.order_status IN ('menunggu pembayaran', 'selesai')
-    AND xs.seller_name IS NULL
-    AND xb.user_id is null
+    AND order_status IN ('menunggu pembayaran', 'selesai')
+    -- standard measure to exclude blacklist
+	AND xs.seller_name IS NULL 
+	AND xb.user_id IS NULL
+    -- The extra measure to exclude ppl
+	AND x.user_id NOT IN (
+        -- Step 0: dataset of users who are internally suspicious but not fall into the sus_buyer database yet
+        SELECT DISTINCT(p.user_id)
+        FROM mock_renos_db.mock_purchase AS p
+        LEFT JOIN mock_renos_db.suspicious_seller AS ss ON ss.seller_name = p.seller_name
+        LEFT JOIN mock_renos_db.suspicious_buyer AS sb ON sb.user_id = p.user_id
+        WHERE p.user_id IN (
+            SELECT r.user_id
+            FROM mock_renos_db.mock_purchase AS r
+            WHERE order_date < '2025-07-21'
+              AND order_status IN ('menunggu pembayaran', 'selesai')
+              AND sku_sell_price / sku_qty <= 500000  -- suspicious low-price transactions
+        )
+        AND p.user_id NOT IN (
+            SELECT user_id
+            FROM mock_renos_db.mock_purchase
+            WHERE order_date >= '2025-07-21'
+              AND order_status IN ('menunggu pembayaran', 'selesai')
+          )
+        AND ss.seller_name IS NULL
+        AND sb.user_id IS NULL
+		-- end of step 0
+    )
+
     GROUP BY x.user_id
-  ) AS user_stats -- end of step 1
-) AS stats_with_quartile -- end of step 2
+  ) AS user_stats
+  -- end of Step 1
+) AS stats_with_quartile
+-- end of Step 2
 WHERE qgroup IN ('q1', 'q2')
-ORDER BY avg_sku_price DESC -- end of step 3
+ORDER BY avg_sku_price DESC
+-- end of Step 3
 ;
