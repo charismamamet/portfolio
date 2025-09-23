dfb331d917f80d29aac893323ca17c2a2b516cf2
charismamamet <charisma.rahmat@gmail.com>
2025-09-23 10:47:45 +0700

    2025-09-23 adding python learning milestones 2025-09-23 adding new apriori usecase with l30d lookback

---
+++ SQL CRM usecases/Queries/apriori_subquery_analysis_l30d_include_exclusions.sql
@@ -0,0 +1,108 @@
+-- step 3 create pivot that is ready to be uploaded to prod catalog
+select 
+	f2.item_a as initial_cat,
+    f2.total_a as initial_orders,
+    max(case when f2.rn = 1 then f2.item_b else null end) as pair_1,
+    max(case when f2.rn = 1 then f2.lift else null end) as lift_1,
+    max(case when f2.rn = 2 then f2.item_b else null end) as pair_2,
+    max(case when f2.rn = 2 then f2.lift else null end) as lift_2,
+    max(case when f2.rn = 3 then f2.item_b else null end) as pair_3,
+    max(case when f2.rn = 3 then f2.lift else null end) as lift_3
+from (
+    -- step 2 create dataset that show the rn
+    select 
+		f1.*,
+    	row_number() over (partition by f1.item_a order by f1.lift desc) as rn
+	from (
+    	-- step 1 create dataset that show item_a and item_b with lift number as a column
+    	SELECT 
+			a.cat_name AS item_a,
+			ta.total_a,
+
+			b.cat_name AS item_b,
+			tb.total_b,  
+  
+			COUNT(*) AS pair_count,
+			tod.total_order_count,
+
+			ROUND(COUNT(*) * 1.0 / tod.total_order_count, 3) AS support,
+			ROUND(COUNT(*) * 1.0 / ta.total_a, 3) AS confidence_A_to_B,
+			ROUND(COUNT(*) * 1.0 / tb.total_b, 3) AS confidence_B_to_A,
+			ROUND((COUNT(*) * 1.0 * tod.total_order_count) / (ta.total_a * tb.total_b), 3) AS lift
+
+		FROM mock_renos_db.mock_purchase AS a
+		JOIN mock_renos_db.mock_purchase AS b ON a.order_id = b.order_id AND a.cat_name <> b.cat_name
+
+		inner JOIN (
+            -- total_a: how many times item_a appears
+  			SELECT cat_name, COUNT(*) AS total_a
+  			FROM mock_renos_db.mock_purchase as a 
+  			left join blacklist_seller as bs on bs.seller_id = a.seller_id
+        	left join suspicious_seller as ss on ss.seller_id = a.seller_id
+        	left join suspicious_buyer as sb on sb.user_id = a.user_id
+  			WHERE 1
+				and a.order_status not in ('dibatalkan')
+				and a.order_date >= date(now()) - interval 30 day
+          	  	and bs.seller_id is null
+        		and ss.seller_id is null
+        		and sb.user_id is null
+  			GROUP BY cat_name
+			-- end of total a
+			) AS ta ON a.cat_name = ta.cat_name
+
+		inner JOIN (
+            -- total_b: how many times item_b appears
+  			SELECT cat_name, COUNT(*) AS total_b
+  			FROM mock_renos_db.mock_purchase as a 
+  			left join blacklist_seller as bs on bs.seller_id = a.seller_id
+        	left join suspicious_seller as ss on ss.seller_id = a.seller_id
+        	left join suspicious_buyer as sb on sb.user_id = a.user_id
+  			WHERE 1
+				and a.order_status not in ('dibatalkan')
+				and a.order_date >= date(now()) - interval 30 day
+          	  	and bs.seller_id is null
+        		and ss.seller_id is null
+        		and sb.user_id is null
+  			GROUP BY cat_name
+			-- end of total_b
+			) AS tb ON b.cat_name = tb.cat_name
+
+		CROSS JOIN (
+            -- total orders
+  			SELECT COUNT(DISTINCT order_id) AS total_order_count
+  			FROM mock_renos_db.mock_purchase as a 
+  			left join blacklist_seller as bs on bs.seller_id = a.seller_id
+        	left join suspicious_seller as ss on ss.seller_id = a.seller_id
+        	left join suspicious_buyer as sb on sb.user_id = a.user_id
+  			WHERE 1
+				and a.order_status not in ('dibatalkan')
+				and a.order_date >= date(now()) - interval 30 day
+          	  	and bs.seller_id is null
+        		and ss.seller_id is null
+        		and sb.user_id is null
+			-- end of total orders
+			) AS tod
+        
+        left join blacklist_seller as bs on bs.seller_id = a.seller_id
+        left join suspicious_seller as ss on ss.seller_id = a.seller_id
+        left join suspicious_buyer as sb on sb.user_id = a.user_id
+
+		WHERE 1
+			and a.order_status not in ('dibatalkan')
+			and a.order_date >= date(now()) - interval 30 day 
+        	and bs.seller_id is null
+        	and ss.seller_id is null
+        	and sb.user_id is null
+
+		GROUP BY item_a, item_b
+		ORDER BY total_a desc, lift DESC
+		-- end of step 1
+		) as f1
+	-- end of step 2
+	) as f2
+where 1
+	and f2.lift >= 1
+group by initial_cat
+order by initial_orders desc
+-- end of step 3
+;
